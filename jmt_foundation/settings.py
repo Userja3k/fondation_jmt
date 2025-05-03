5d4936325cc4542bb9bbbae924defba65d4b9109
Userja3k <israelbrt123@gmail.com>
2025-05-03 06:52:22 +0200

    modifs

--- jmt_foundation/settings.py
+++ jmt_foundation/settings.py
@@ -75,11 +75,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jmt_foundation.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
